ab6171387ccfe6052b7f4bb495df6cb7a85a7a71
adamw <adam@warski.org>
2014-05-02 12:51:24 +0200

    Dockerfile, comments

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-docker-spray-example
-====================
+Docker + Spray example
+======================
+
+1. Get [Docker](http://www.docker.io)
+2. Get [SBT](http://www.scala-sbt.org/)
+3. Run `sbt assembly` - this will create a fat-jar with the server
+4. Build the docker image: `docker build .`. Note the final image id.
+5. Run a container basing on the image, remapping the ports: `docker run --rm -p 9090:8080 [image id]`
+6. Enjoy! Example requests:
+
+    curl "http://localhost:9090/hello"
+    curl "http://localhost:9090/counter/c1"
+    curl -XPOST "http://localhost:9090/counter/c1?amount=1234"
+
+You can also get a ready image from the [Docker index](https://index.docker.io/u/adamw/spray-example/).
--- src/main/scala/com/softwaremill/example/DockedServer.scala
+++ src/main/scala/com/softwaremill/example/DockedServer.scala
@@ -7,32 +7,36 @@ import akka.util.Timeout
 import scala.concurrent.duration._
 
 object DockedServer extends App with SimpleRoutingApp {
+  // setup
   implicit val actorSystem = ActorSystem()
-
   implicit val timeout = Timeout(1.second)
   import actorSystem.dispatcher
 
+  // an actor which holds a map of counters which can be queried and updated
   val countersActor = actorSystem.actorOf(Props(new CountersActor()))
 
-  startServer(interface = "localhost", port = 8080) {
+  startServer(interface = "0.0.0.0", port = 8080) {
+    // definition of how the server should behave when a request comes in
+
+    // simplest route, matching only GET /hello, and sending a "Hello World!" response
     get {
       path("hello") {
         complete {
           "Hello World!"
         }
       }
-    } ~
-    path("counter" / Segment) { counterName =>
+    } ~ // the ~ concatenates two routes: if the first doesn't match, the second is tried
+    path("counter" / Segment) { counterName =>  // extracting the second path component into a closure argument
       get {
         complete {
-          (countersActor ? Get(counterName))
+          (countersActor ? Get(counterName)) // integration with futures
             .mapTo[Int]
             .map(amount => s"$counterName is: $amount")
         }
       } ~
       post {
-        parameters("amount".as[Int]) { amount =>
-          countersActor ! Add(counterName, amount)
+        parameters("amount".as[Int]) { amount => // the type of the amount closure argument is Int, as specified!
+          countersActor ! Add(counterName, amount) // fire-and-forget
           complete {
             "OK"
           }
@@ -41,6 +45,7 @@ object DockedServer extends App with SimpleRoutingApp {
     }
   }
 
+  // implementation of the actor
   class CountersActor extends Actor {
     private var counters = Map[String, Int]()
 
@@ -52,6 +57,7 @@ object DockedServer extends App with SimpleRoutingApp {
     }
   }
 
+  // messages to communicate with the counters actor
   case class Get(counterName: String)
   case class Add(counterName: String, amount: Int)
 }
